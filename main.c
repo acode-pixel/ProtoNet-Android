bcccad71ab13ea44cb2efdf4d2683b0cd47f552c
acode.pixel <donaldanthony245@gmail.com>
2023-11-09 16:01:20 +0800

    new file:   main.c 	modified:   notes.txt

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+	struct Packet {
+		char Proto[4];
+		char Mode[1];
+		int IP;
+
+		uint8_t data[1024-9];
+	};
+	
+	struct Packet *test = NULL;
+	test = (struct Packet*) malloc(sizeof(struct Packet));
+	uint8_t* data = "BROD\xff\xff\xff\xff\x0a";
+	memset(test, 0, sizeof(*test));
+	memcpy(test->Proto, (char*)"SPTP", sizeof(int));
+	memcpy(test->Mode, (char*)"\x01", sizeof(char));
+	memcpy(&test->IP, (int*)"\xa3\xff\x00\x24", sizeof(int));
+	memcpy(test->data, data, sizeof(data)+1);
+
+	printf("%lu\n", sizeof(*test));
+
+	FILE* f = fopen("mem.bin", "wb");
+	fwrite(test, 1, 1024, f);
+	fclose(f);
+
+
+
+	return 0;
+}
